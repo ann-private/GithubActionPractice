1287dc5632e656253c60ddfd347cd6a6c060a78c
WeiWW <wei.juin.lin@gmail.com>
2024-04-15 08:08:11 +0900

    testing properties

--- sample.sh
+++ sample.sh
@@ -1,1 +1,14 @@
-echo "Shell script.Hello, World!"
+set -e
+GIT_TAG=$(git describe --abbrev=0)
+./gradlew clean documentation:generatePublishableDocs
+if [ [ ! $GIT_TAG == *"-"* ] ]; then
+  git fetch --no-tags origin gh-pages
+  git checkout gh-pages
+  cp -R documentation/build/publishableDocs/. ./docs
+  git add docs
+  git config user.name "Travis CI"
+  git config user.email "email"
+  git commit -m "Update documentation for $GIT_TAG"
+  git push origin gh-pages
+
+fi
